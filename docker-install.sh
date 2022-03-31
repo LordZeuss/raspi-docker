bbc0ed3831d9f5f337813c50766426a2e76ec62d
LordZeuss <87180353+LordZeuss@users.noreply.github.com>
2022-03-31 03:56:32 -0500

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This is a script to help install Mullvad VPN for the Raspberry pi
+#This is a script to help install docker and docker-compose files for the Raspberry pi
 
 ######################################################################
 
@@ -15,7 +15,8 @@ update () { yes | sudo apt-get update && sudo apt-get upgrade; }
 #System Update
 #
 
-echo "Would you like to update the system (Recommended)? (y/n)"
+echo "Would you like to update the system (Recommended)? (y/n/e)"
+echo "y is to confirm, n is to skip, e is to exit the installer."
 
 read yesorno
 
@@ -34,18 +35,40 @@ fi
 #Docker Script Install
 #
 
-echo "Would you like to install Docker (Required)? (y/n)"
+echo "Would you like to install Docker (Required)? (y/n/e)"
 
 read yesorno
 
 if [ "$yesorno" = y ]; then
-	curl -fsSL https://get.docker.com -o get-docker.sh
-	sudo sh get-docker.sh
-	sudo usermod -aG docker $USER
-	sudo usermod -aG docker Pi
+	yes | sudo apt-get install docker.io
+	sudo systemctl start docker
+	sudo systemctl enable docker
 	echo "Installation Successful."
 elif [ "$yesorno" = n ]; then
 	echo "Skipping..."
 else
 	echo "Not a valid answer. Exiting..."
+	exit 1
 fi
+
+
+
+#Docker Compose Install
+
+echo "Would you like to install Docker Compose (Required)? (y/n/e)"
+
+read yesorno
+
+if [ "$yesorno" = y ]; then
+	yes | sudo apt install docker-compose
+	sudo groupadd docker
+	sudo gpasswd -a ${USER} docker
+	su - $USER
+	echo "Installation Successful."
+elif [ "$yesorno" = n ]; then
+	echo "Skipping..."
+else
+	echo "Not a valid answer. Exiting..."
+	exit 1
+fi
+
