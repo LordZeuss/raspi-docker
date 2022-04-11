8affd4ca44a0e957542c8c4348321d098de4d5e6
LordZeuss <87180353+LordZeuss@users.noreply.github.com>
2022-04-11 03:40:48 -0500

    Update docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -2,6 +2,6 @@
 
 # This script is to simply automate the command to start docker-compose. Some may find it easier to use the actual command, others may prefer to just run this script.
 
-
+clear
 echo "Starting Docker-compose..."
 sudo docker-compose up -d
